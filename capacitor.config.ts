7d737af5318c6c28e5c0158cccf7256324a564ac
codedestroyer19 <mikekiwalabye@gmail.com>
2023-05-27 11:09:21 +0200

    first commit: scaffold

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.goofy_ahh',
+  appName: 'goofy_ahh',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
